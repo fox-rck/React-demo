dda785959cb99ada74f02b0f8fa6dce1c7adf334
Sam S (Rogue Dangler) <sam.swett1@searshc.com>
2015-04-29 11:49:45 -0700

    fix test setup

--- __tests__/TestWorld.spec.js
+++ __tests__/TestWorld.spec.js
@@ -4,14 +4,14 @@ describe('awesomeFn', function() {
  	var React, TestUtils, TestWorldComponent, TestWorld;
  	
  	beforeEach(function() {
- 		React = require('react/addons');
-		TestWorldComponent = require('../src/scripts/test_world/TestWorld');
-		TestUtils = React.addons.TestUtils;
-    	TestUtils.renderIntoDocument(<TestWorld />);
+//  		React = require('react/addons');
+// 		TestWorldComponent = require('../src/scripts/test_world/TestWorld');
+// 		TestUtils = React.addons.TestUtils;
+// 	   	TestUtils.renderIntoDocument(<TestWorld />);
  	});
  	
 	it('should return false', function() {
-	console.log(TestWorldComponent);
+		console.log(TestWorldComponent);
 		//expect(TestWorldComponent.awesomeFn()).toBe(false);
 	});
 });
--- __tests__/sum.spec.js
+++ __tests__/sum.spec.js
@@ -1,9 +1,9 @@
 // __tests__/sum-test.js
-jest.dontMock('../sum');
+jest.dontMock('../src/scripts/test_world/sum.js');
 
 describe('sum', function() {
  it('adds 1 + 2 to equal 3', function() {
-   var sum = require('../sum');
+   var sum = require('../src/scripts/test_world/sum.js');
    expect(sum(1, 2)).toBe(3);
  });
 });
